cf92fdbe9fac53414ed8424a4e63c1d5f4e49c6d
Sky King <skyking@gmail.com>
2014-07-25 16:07:27 -0700

    Add comments, reformat code, update test code

--- proj2/cachematrix.R
+++ proj2/cachematrix.R
@@ -1,39 +1,93 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file uses memoization (http://en.wikipedia.org/wiki/Memoization)
+## to implement a cached version of a funciton to compute matrix inversion.
+## If the input matrix has not changed since the last time the inverse was 
+## calculated, we do not recompute the inverse and instead return the cached
+## version.  If the matrix does change, we will recompute the inverse.
 
-## Write a short comment describing this function
+## makeCacheMatrix(matrix)
+## wrapper around a standard R matrix, proving getter and setter methods,
+## and getinverse and setinverse methods to store a cached version of the 
+## inverse.  If the underlying matrix is changed via its setter, the cached
+## inverse is invalidated
 
-makeCacheMatrix <- function(m = matrix()) {
-    inverse <- NULL
-    set <- function(y) {
+makeCacheMatrix <- function(m = matrix()) 
+{
+    inverse <- NULL # inverse starts uncomputed
+    
+    ## set(matrix)
+    ## sets the underlying matrix and invalidates the cached inverse
+    set <- function(y) 
+    {
         m <<- y
         inverse <<- NULL
     }
-    get <- function() m
-    setinverse <- function(inv) inverse <<- inv
-    getinverse <- function() inverse
+    
+    ## get()
+    ## return the underlying matrix
+    get <- function()
+    {
+        m
+    }
+    
+    ## setinverse(matrix)
+    ## sets (but does not compute!) the value of the inverse of the underlying
+    ## matrix
+    setinverse <- function(inv) 
+    {
+        inverse <<- inv
+    }
+    
+    ## return the cached value of the inverse of the matrix.
+    ## returns NULL if the inverse has not yet be set, or the underlying
+    ##   matrix has changed since the last invocation
+    getinverse <- function() 
+    {
+        inverse
+    }
+    
+    ## return a list of getter and setter methods as a result of object creation
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)    
 }
 
 
-## Write a short comment describing this function
+## cacheSolve(cacheMatrix, ...)
+## Returns the inverse of the passed in cacheMatrix object
+## If the inverse has already been calculated and the matrix not changed
+## since the last invocation, returns the cached version.
+## If the inverse has not yet been calculated or the underlying matrix 
+## changed since the last call, (re)computes the value with a call to solve(...)
 
 cacheSolve <- function(m, ...) {
     ## Return a matrix that is the inverse of 'm'
     inv <- m$getinverse()
-    if(!is.null(inv)) {
+    if(!is.null(inv)) 
+    {
+        ## Inverse already calculated, value not stale
         message("getting cached data")
         return(inv)
     }
+    
+    ## Either inverse has never been calculated or cached inverse is stale
     data <- m$get()
     inv <- solve(data, ...)
     m$setinverse(inv)
     inv    
 }
 
-test <- function(m) {
+## Test function to exercise above code.  Would be better written as a unit test
+## using RUnit.
+## A matrix multiplied by it's inverse will equal the identity matrix.
+## 
+## for a 2x2 matrix this will look like
+## getting cached data
+##       [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+
+test <- function() 
+{
     amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
     amatrix$get()         # Returns original matrix
     cacheSolve(amatrix)   # Computes, caches, and returns    matrix inverse
@@ -44,4 +98,6 @@ test <- function(m) {
     cacheSolve(amatrix)   # Computes, caches, and returns new matrix inverse
     amatrix$get()         # Returns matrix
     amatrix$getinverse()  # Returns matrix inverse    
+    
+    amatrix$get() %*% amatrix$getinverse() # returns the identity matrix
 }

d44e0ecd5b97d7a9b6b9576a1808180f4ea3066f
Stephanie Hudson <shudson@panaceasystems.org>
2016-05-02 14:56:30 -0700

    wk3 matrix assignment #2

--- proj2/cachematrix.R
+++ proj2/cachematrix.R
@@ -10,70 +10,31 @@
 ## inverse.  If the underlying matrix is changed via its setter, the cached
 ## inverse is invalidated
 
-makeCacheMatrix <- function(m = matrix()) 
-{
-    inverse <- NULL # inverse starts uncomputed
-    
-    ## set(matrix)
-    ## sets the underlying matrix and invalidates the cached inverse
-    set <- function(y) 
-    {
-        m <<- y
-        inverse <<- NULL
-    }
-    
-    ## get()
-    ## return the underlying matrix
-    get <- function()
-    {
-        m
-    }
-    
-    ## setinverse(matrix)
-    ## sets (but does not compute!) the value of the inverse of the underlying
-    ## matrix
-    setinverse <- function(inv) 
-    {
-        inverse <<- inv
-    }
-    
-    ## return the cached value of the inverse of the matrix.
-    ## returns NULL if the inverse has not yet be set, or the underlying
-    ##   matrix has changed since the last invocation
-    getinverse <- function() 
-    {
-        inverse
-    }
-    
-    ## return a list of getter and setter methods as a result of object creation
-    list(set = set, get = get,
-         setinverse = setinverse,
-         getinverse = getinverse)    
+makeCacheMatrix <- function(m = matrix()) {
+        inverse <- NULL
+        set <- function(y) {
+                m <<- y
+                inverse <<- NULL
+        }
+        get <- function() m
+        setinverse <- function(mtx) inverse <<- mtx
+        getinverse <- function() inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## cacheSolve(cacheMatrix, ...)
-## Returns the inverse of the passed in cacheMatrix object
-## If the inverse has already been calculated and the matrix not changed
-## since the last invocation, returns the cached version.
-## If the inverse has not yet been calculated or the underlying matrix 
-## changed since the last call, (re)computes the value with a call to solve(...)
-
 cacheSolve <- function(m, ...) {
-    ## Return a matrix that is the inverse of 'm'
-    inv <- m$getinverse()
-    if(!is.null(inv)) 
-    {
-        ## Inverse already calculated, value not stale
-        message("getting cached data")
-        return(inv)
-    }
-    
-    ## Either inverse has never been calculated or cached inverse is stale
-    data <- m$get()
-    inv <- solve(data, ...)
-    m$setinverse(inv)
-    inv    
+        inv <- m$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- m$get()
+        inv <- solve(data, ...)
+        m$setinverse(inv)
+        inv
 }
 
 ## Test function to exercise above code.  Would be better written as a unit test
